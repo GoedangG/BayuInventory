cc17d7ee9c30268a156daeaba66eea8680c199eb
GoedangG <rizqi.bayu@ui.ac.id>
2023-09-24 19:43:27 +0700

    replace gitignore

--- main/models.py
+++ main/models.py
@@ -5,3 +5,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     date = models.DateField(auto_now_add= True)
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    hobby = models.TextField()
