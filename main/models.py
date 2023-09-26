6612541d2495cdd6dc1bb43cb1a58c468c5f485a
GoedangG <rizqi.bayu@ui.ac.id>
2023-09-26 18:36:29 +0700

    Merubah price menjadi amount

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField()
     description = models.TextField()
     date = models.DateField(auto_now_add= True)
 
