be767a7165ae92d87afbac62f9ade4afe9cb4540
GoedangG <rizqi.bayu@ui.ac.id>
2023-11-23 13:19:56 +0700

    Integrasi

--- BayuInventory/settings.py
+++ BayuInventory/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'main',
     'authentication',
     'corsheaders',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
 
 @csrf_exempt
 def login(request):
@@ -14,21 +12,38 @@ def login(request):
     if user is not None:
         if user.is_active:
             auth_login(request, user)
-            # Status login sukses.
+            # Successful login status.
             return JsonResponse({
                 "username": user.username,
                 "status": True,
-                "message": "Login sukses!"
-                # Tambahkan data lainnya jika ingin mengirim data ke Flutter.
+                "message": "Login successful!"
+                # Add other data if you want to send data to Flutter.
             }, status=200)
         else:
             return JsonResponse({
                 "status": False,
-                "message": "Login gagal, akun dinonaktifkan."
+                "message": "Login failed, account disabled."
             }, status=401)
 
     else:
         return JsonResponse({
             "status": False,
-            "message": "Login gagal, periksa kembali email atau kata sandi."
+            "message": "Login failed, check email or password again."
+        }, status=401)
+    
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logged out successfully!"
+        }, status=200)
+    except:
+        return JsonResponse({
+        "status": False,
+        "message": "Logout failed."
         }, status=401)
